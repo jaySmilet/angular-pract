daf5611776d2c1d8eec0445ee4e2cf5a7babf4a1
jaySmilet <jayganeshgupta0@gmail.com>
2022-08-30 23:57:18 +0530

    3 files modified, 1 added

--- src/app/ng-temp/ng-temp-child/ng-temp-child.component.ts
+++ src/app/ng-temp/ng-temp-child/ng-temp-child.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, TemplateRef } from '@angular/core';
 
 @Component({
   selector: 'app-ng-temp-child',
   templateUrl: './ng-temp-child.component.html',
-  styleUrls: ['./ng-temp-child.component.css']
+  styleUrls: ['./ng-temp-child.component.css'],
 })
 export class NgTempChildComponent implements OnInit {
+  @Input() customTemplate: TemplateRef<HTMLElement>;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
--- src/app/ng-temp/ng-temp.component.ts
+++ src/app/ng-temp/ng-temp.component.ts
@@ -1,17 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  OnInit,
+  TemplateRef,
+  ViewChild,
+  ViewContainerRef,
+} from '@angular/core';
 
 @Component({
   selector: 'app-ng-temp',
   templateUrl: './ng-temp.component.html',
-  styleUrls: ['./ng-temp.component.css']
+  styleUrls: ['./ng-temp.component.css'],
 })
-export class NgTempComponent implements OnInit {
+export class NgTempComponent implements OnInit, AfterViewInit {
+  @ViewChild('parentTemplate') myTemplate: TemplateRef<HTMLElement>;
+  @ViewChild('sayHelloTemplate', { read: TemplateRef })
+  sayHelloTemplate: TemplateRef<any>;
+  selected;
 
-  constructor() { }
+  constructor(private vref: ViewContainerRef) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  ngAfterViewInit() {
+    console.log(this.myTemplate);
+    console.log(this.vref.createEmbeddedView(this.sayHelloTemplate));
   }
-
 }
 
 // Sources:-https://www.tektutorialshub.com/angular/ngtemplateoutlet-in-angular/
+// Sources:-https://www.tektutorialshub.com/angular/ng-template-in-angular/
