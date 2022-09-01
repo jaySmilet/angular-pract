1d0d22108d95218fd2c00c5198c52c11476e8dda
jaySmilet <jayganeshgupta0@gmail.com>
2022-09-01 16:51:24 +0530

    2 files modified

--- src/app/view-c/view-c.component.ts
+++ src/app/view-c/view-c.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import { ViewChComponent } from './view-ch/view-ch.component';
 
 @Component({
   selector: 'app-view-c',
   templateUrl: './view-c.component.html',
-  styleUrls: ['./view-c.component.css']
+  styleUrls: ['./view-c.component.css'],
 })
-export class ViewCComponent implements OnInit {
-
-  constructor() { }
+export class ViewCComponent implements OnInit, AfterViewInit {
+  // @ViewChild(ViewChComponent, { static: true }) child: ViewChComponent;
+  @ViewChild('usingChildTemRef', { static: true }) child: ViewChComponent;
+  @ViewChild('tempRef', { static: false }) tempRef: ElementRef;
+  showCounter:true;
+  constructor() {
+  }
 
   ngOnInit() {
   }
 
+  increment() {
+    this.child.increment();
+  }
+
+  decrement() {
+    this.child.decrement();
+  }
+
+  ngAfterViewInit() {
+    console.log(this.tempRef.nativeElement.value);
+  }
 }
